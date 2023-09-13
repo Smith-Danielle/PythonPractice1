95a392bf79764a3442aa188d95cb00dceab0ecb6
Danielle Smith <smith.ddms@gmail.com>
2023-09-13 03:52:48 -0500

    Python Codewars Challenge for 9.12.23.

--- main.py
+++ main.py
@@ -202,3 +202,29 @@ def solution(s):
     if len(temp) > 0:
         sliced += temp[::-1]
     return sliced
+
+def check_vin(number):
+    if len(number) != 17 or "I" in number or "O" in number or "Q" in number:
+        return False
+
+    conversion = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "J": 1, "K": 2,
+                  "L": 3, "M": 4, "N": 5, "P": 7, "R": 9, "S": 2, "T": 3, "U": 4, "V": 5, "W": 6,
+                  "X": 7, "Y": 8, "Z": 9}
+    weights = [8,7,6,5,4,3,2,10,0,9,8,7,6,5,4,3,2]
+    converted = 0
+
+    for x in range(len(number)):
+        if number[x].isalpha() and number[x].islower():
+            return False
+        if number[x].isnumeric():
+            converted += int(number[x]) * weights[x]
+        else:
+            converted += conversion[number[x]] * weights[x]
+
+    if number[8].isnumeric() and converted % 11 == int(number[8]):
+        return True
+    if converted % 11 == 10 and number[8] == "X":
+        return True
+    return False
+
+print(check_vin("5YJ3E1EA7HF000337"))
